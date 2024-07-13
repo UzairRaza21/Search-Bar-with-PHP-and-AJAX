01e11b558b92150c08e496401810e549bd9ae0b2
Uzair-Raza <uzairraza0@gmail.com>
2024-07-13 15:23:26 +0500

    update

--- adlisting.sql
+++ adlisting.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 10, 2024 at 02:03 PM
+-- Generation Time: Jul 13, 2024 at 12:22 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -55,19 +55,22 @@ CREATE TABLE `ads` (
   `ad_commission` varchar(255) NOT NULL,
   `ad_img` varchar(255) NOT NULL,
   `ad_city` varchar(255) NOT NULL,
-  `ad_zipcode` int(11) NOT NULL,
+  `ad_zipcode` varchar(255) NOT NULL,
   `ad_bedroom` varchar(255) NOT NULL,
   `ad_bathroom` varchar(255) NOT NULL,
   `ad_agent_name` varchar(255) NOT NULL,
-  `ad_agent_phone` varchar(255) NOT NULL
+  `ad_agent_phone` varchar(255) NOT NULL,
+  `ad_category` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `ads`
 --
 
-INSERT INTO `ads` (`ad_id`, `ad_address`, `ad_price`, `ad_size`, `ad_year`, `ad_commission`, `ad_img`, `ad_city`, `ad_zipcode`, `ad_bedroom`, `ad_bathroom`, `ad_agent_name`, `ad_agent_phone`) VALUES
-(18, '602 Bellemeade BLVD USA 602 Bellemeade BLVD USA 602 Bellemeade BLVD USA ', '100,000', '100', '2024', '10000', 'dash-bg.jpg', 'Dallas', 1234, '3', '3', 'James', '1234');
+INSERT INTO `ads` (`ad_id`, `ad_address`, `ad_price`, `ad_size`, `ad_year`, `ad_commission`, `ad_img`, `ad_city`, `ad_zipcode`, `ad_bedroom`, `ad_bathroom`, `ad_agent_name`, `ad_agent_phone`, `ad_category`) VALUES
+(36, '602 Bellemeade BLVD', '100,000', '100', '2024', '10000', '[\"dash-bg.jpg\"]', 'Dallas', '72467', '3', '2', 'James', '1234', 'sale'),
+(37, '602 Bellemeade BLVD', '100,000', '100', '2024', '10000', '[\"dash-bg.jpg\"]', 'Dallas', '72467', '3', '2', 'James', '1234', 'sale'),
+(38, '602 Bellemeade BLVD', '100,000', '100', '2024', '1,000', '[\"dash-bg.jpg\"]', 'Mexico', '72467', '3', '2', 'James', '1234', 'lease');
 
 --
 -- Indexes for dumped tables
@@ -99,7 +102,7 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `ads`
 --
 ALTER TABLE `ads`
-  MODIFY `ad_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `ad_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
